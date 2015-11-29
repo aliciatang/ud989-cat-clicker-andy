af86bcf1657af53641796cf4f8f81430b8a30351
Alicia Tang <alicia.x.tang@gmail.com>
2015-11-28 21:34:22 -0800

    mvo implementation with underscore

--- app.js
+++ app.js
@@ -1,36 +1,77 @@
+$(function() {
+  var model = {
+    cats: [
+      { name: "Andy", image: "cat_picture1.jpg"},
+      { name: "Boby", image: "cat_picture2.jpeg"},
+      { name: "Cindy", image: "cat_picture3.jpeg"},
+      { name: "Derek", image: "cat_picture4.jpeg"},
+      { name: "Frank", image: "cat_picture5.jpeg"}
+    ],
 
-var cats = $(".cat");
-var buttons = $("button");
+    init: function() {
+            this.current = 0;
+            model.cats.forEach(function(cat, i) { model.cats[i]['counter'] = 0});
+          },
+    increaseCurrent: function() {
+            model.cats[this.current].counter ++;
+          },
+    getCurrent: function() {
+            return model.cats[this.current];
+          },
+    setCurrent: function(i) {
+            this.current = i;
+          }
+  };
 
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
-	}
-}
+  var octopus = {
+    increase: function() {
+            model.increaseCurrent();
+            catView.render();    
+          },
+    getCats: function() {
+            return model.cats;
+          },
+    getCat: function() {
+            return model.getCurrent();
+            },
+    init: function() {
+            model.init();
+            catsView.init();
+            catView.init();
+          },
+    setCurrent: function(i) {
+            model.setCurrent(i);
+            catView.render();
+          },
+  };
 
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
-}
+  var catsView = {
+    init: function() {
+            this.buttonsContainer = $('#buttons');
+            this.buttonTemp = _.template($('script.template#button').html(), {variable: "cats"});
+            catsView.render();
+          },
+    render: function() {
+            this.buttonsContainer.html(this.buttonTemp(octopus.getCats()));
+            this.buttonsContainer.find('button').click(function(){
+              octopus.setCurrent($('button').index(this));
+            });
+          }
+  };
 
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
-}
+  var catView = {
+    init: function() {
+            this.catContainer = $('#cat');
+            this.catTemp = _.template($('script.template#panel').html(), {variable: "cat"});
+            catView.render();
+          },
+    render: function() {
+            this.catContainer.html(this.catTemp(octopus.getCat()));
+            this.catContainer.find('img').click(function(){
+                octopus.increase();
+            });
+          },
+  };
 
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
-}
-
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
-}
-
-hideAllCats();
-$("#cat1").show();
+  octopus.init();
+});
